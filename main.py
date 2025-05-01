5bd12cf0bc7044f08fda13598149e5fdaed72ddd
Aneprados <apradoscamara@gmail.com>
2025-05-01 12:02:07 +0100

    Esta casi listo

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import sys
-
+from Lanzador import Lanzador
 DATABASE_PATH = 'clientes.csv'
 if 'pytest' in sys.argv[0]:
     DATABASE_PATH = 'tests/clientes_test.csv'
 
-# run.py
-from Lanzador import Lanzador
+
 
 if __name__ == "__main__":
     Lanzador.iniciar()
